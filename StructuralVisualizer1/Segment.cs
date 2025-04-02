9c82027efe76328eeef4eb45964f55d30efcc95a
Mark <>
2025-04-02 16:22:17 -0600

    More segment tests

--- StructuralVisualizer1/Segment.cs
+++ StructuralVisualizer1/Segment.cs
@@ -2,6 +2,7 @@ namespace StructuralVisualizer;
 
 public class Segment(Point start, Point end)
 {
+
     public Point Start { get; private set; } = start;
     public Point End { get; private set; } = end;
 
--- UnitTestsStructural/SegmentTest.cs
+++ UnitTestsStructural/SegmentTest.cs
@@ -1,3 +1,4 @@
+using System.Net.Mail;
 using StructuralVisualizer;
 
 namespace UnitTestsStructural
@@ -16,6 +17,23 @@ namespace StructuralVisualizer1
 
 
             Assert.Throws<CustomExceptions.TparamException>(() => testSegment.PointAt(56.7));
+        }
+
+            [Fact]
+        public void TestPointAt()
+        {
+            Segment testSegment = new Segment(new Point(400, 0), new Point(0, 400));
+            double t = new TParam().Make(0.25);
+            Point expected = new Point(300, 100);
+            Point actual = testSegment.PointAt(t);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestMiddlePoint()
+        {
+            Segment testSegment = new Segment(new Point(400, 0), new Point(0, 400));
+            Assert.Equal(new Point(200,200),testSegment.Middle);
         }
     }
 }
