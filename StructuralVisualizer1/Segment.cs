106a0f0c4af25c4373a5dddec621b00ef12aab72
Mark <>
2025-04-02 17:02:59 -0600

    Added closest point to Segments

--- StructuralVisualizer1/Segment.cs
+++ StructuralVisualizer1/Segment.cs
@@ -21,6 +21,17 @@ public class Segment(Point start, Point end)
     }
 
     public Point Middle => PointAt(new TParam().Middle);
-    
+
+    public Point ClosestPointTo(Point p)
+    {
+        Vector v = new VectorFactory().MakeVectorBetween(Start, p);
+        Vector d = DirectionVersor;
+        double vs = v.ProjectionOver(d);
+        
+        return vs < 0 ? Start : (vs > 0 ? End : Start.Displaced(d,vs));
+        
+    }
+
+    public double DistanceTo(Point p) => p.DistanceTo(ClosestPointTo(p), p);
 
 }
